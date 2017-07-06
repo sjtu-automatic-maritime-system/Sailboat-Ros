515de60424128584c21bd8ad208d34ac96333743
hywel1994 <xiehq1994@163.com>
2017-07-06 14:36:43 +0800

    little change

--- sh/fleet_race_simulation.sh
+++ sh/fleet_race_simulation.sh
@@ -1,5 +1,5 @@
 nohup roslaunch sailboat_launch start_tf_tree_onboat.launch &
 nohup roslaunch sailboat_simulation fleet_race_simulation.launch &
 nohup roslaunch sailboat_launch interface_onshore.launch &
-echo "start scanning simulation"
+echo "start fleet_race simulation"
 rosnode list
