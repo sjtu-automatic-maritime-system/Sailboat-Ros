2b1a7a1c4c19d4a9ecb05ec0c2e9412eb6d6bc14
hywel1994 <xiehq1994@163.com>
2017-09-06 14:31:26 +0800

    modify scanning

--- FUNCTION/scanning/src/rt_nonfinite.cpp
+++ FUNCTION/scanning/src/rt_nonfinite.cpp
@@ -3,9 +3,9 @@
 //
 // Code generated for Simulink model 'scanning'.
 //
-// Model version                  : 1.277
+// Model version                  : 1.290
 // Simulink Coder version         : 8.6 (R2014a) 27-Dec-2013
-// C/C++ source code generated on : Wed Aug 23 14:22:40 2017
+// C/C++ source code generated on : Wed Sep 06 06:37:15 2017
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: 32-bit Generic
