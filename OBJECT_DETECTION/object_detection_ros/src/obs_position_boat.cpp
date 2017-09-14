8fbe089d652c03a1df08718e98e8145ed3c4f0ca
StarsMyDestination <xujianyun101@gmail.com>
2017-09-15 02:12:57 +0800

    get obs postion

--- OBJECT_DETECTION/object_detection_ros/src/obs_position_boat.cpp
+++ OBJECT_DETECTION/object_detection_ros/src/obs_position_boat.cpp
@@ -17,7 +17,7 @@
 #include "tld_msgs/BoundingBox.h"
 #include "sailboat_message/Ahrs_msg.h"
 #include "sailboat_message/WTST_msg.h"
-#include "geometry_msgs/Point.h"
+#include "geometry_msgs/PointStamped.h"
 
 #define IMG_WIDTH 1296
 #define IMG_HEIGHT 964
@@ -40,13 +40,17 @@ void callback(const tld_msgs::BoundingBoxConstPtr &bbox_msg) {
     int x_center = x + width / 2;
     int y_center = y + height / 2;
 
-    double direction = (x_center - IMG_HEIGHT/2) / IMG_WIDTH * FOV;
-    double distance = (IMG_HEIGHT - y_center) / PIXELS_TO_BOW * DISTANCE_TO_BOW;
+    double direction = (x_center - IMG_HEIGHT / 2) / (double) IMG_WIDTH * FOV;
+    double distance = (IMG_HEIGHT - y_center) / (double) PIXELS_TO_BOW * DISTANCE_TO_BOW;
 
-    geometry_msgs::Point obs_pos;
-    obs_pos.x = distance*cos(direction/57.3);
-    obs_pos.y = distance*sin(direction/57.3);
-    obs_pos.z = 0;
+    geometry_msgs::PointStamped obs_pos;
+    obs_pos.header = bbox_msg->header;
+    obs_pos.header.frame_id = "WTST";
+    obs_pos.point.x = distance * cos(direction / 57.3);
+    obs_pos.point.y = distance * sin(direction / 57.3);
+    obs_pos.point.z = 0;
+
+    std::cout << "obs_pos x: " << obs_pos.point.x << "  y: " << obs_pos.point.y << endl;
 
     obs_pub.publish(obs_pos);
 
@@ -55,10 +59,10 @@ void callback(const tld_msgs::BoundingBoxConstPtr &bbox_msg) {
 
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "undistort_image");
+    ros::init(argc, argv, "obs_position");
     ros::NodeHandle nh;
 
-    obs_pub = nh.advertise<geometry_msgs::Point>("/obs_position", 2);
+    obs_pub = nh.advertise<geometry_msgs::PointStamped>("/obs_position", 2);
     ros::Subscriber bbox_sub = nh.subscribe("/tld_tracked_object", 2, &callback);
 
     ros::spin();
---
+++ PYTHON/get_path.py
@@ -0,0 +1,130 @@
+import rospy
+from sailboat_message.msg import WTST_msg
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped, Point
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import PointStamped
+import message_filters
+
+
+target_x = [0, 10]
+target_y = [0, 10]
+
+marker = Marker()
+marker.header.frame_id = 'WTST'
+marker.type = marker.POINTS
+marker.action = marker.ADD
+marker.pose.orientation.w = 1
+marker.scale.x = 5.0
+marker.scale.y = 5.0
+marker.scale.z = 5.0
+marker.color.r = 1.0
+marker.color.g = 1.0
+marker.color.b = 0
+marker.color.a = 0.5
+
+for xx, yy in zip(target_x, target_y):
+    p = Point()
+    p.x = xx
+    p.y = yy
+    marker.points.append(p)
+
+count = 0
+path_ego = Path()
+path_obs = Path()
+
+# obs_pos = PointStamped()
+# obs_pos.point.x
+
+# def callback(wtst_msg, obs_pos):
+#     global count
+#     po_ego = PoseStamped()
+#     po_ego.header = wtst_msg.header
+#     # po.pose.position.x = wtst_msg.PosX
+#     # po.pose.position.y = wtst_msg.PosY
+#     po_ego.pose.position.x = wtst_msg.PosY  ## change x and y to plot
+#     po_ego.pose.position.y = wtst_msg.PosX
+#     path_ego.poses.append(po_ego)
+#
+#     po_obs = PoseStamped()
+#     po_obs.header = wtst_msg.header
+#     # po_obs.position.x = obs_pos.x
+#     # po_obs.position.y = obs_pos.y
+#     po_obs.pose.position.x = obs_pos.point.y  ## change x and y to plot
+#     po_obs.pose.position.y = obs_pos.point.y
+#     path_obs.poses.append(po_obs)
+#
+#
+#     if count == 10:  # no need to publish path every callback, or the rviz would be very slow
+#         path_ego.header = wtst_msg.header
+#         path_pub_ego.publish(path_ego)
+#         path_obs.header = wtst_msg.header
+#         # path_obs.header.frame_id = 'WTST'
+#         path_pub_obs.publish(path_obs)
+#         marker.header = wtst_msg.header
+#         points_pub.publish(marker)
+#         count = 0
+#     count += 1
+
+
+def ego_cb(wtst_msg):
+    global count
+    po_ego = PoseStamped()
+    po_ego.header = wtst_msg.header
+    # po.pose.position.x = wtst_msg.PosX
+    # po.pose.position.y = wtst_msg.PosY
+    po_ego.pose.position.x = wtst_msg.PosY  ## change x and y to plot
+    po_ego.pose.position.y = wtst_msg.PosX
+
+    if count == 10:  # no need to publish path every callback, or the rviz would be very slow
+        path_ego.poses.append(po_ego)
+        path_ego.header = wtst_msg.header
+        path_pub_ego.publish(path_ego)
+        marker.header = wtst_msg.header
+        points_pub.publish(marker)
+        count = 0
+    count += 1
+
+#
+def obs_cb(obs_pos):
+    po_obs = PoseStamped()
+    po_obs.header = po_obs.header
+    po_obs.header.frame_id = 'WTST'
+    # po_obs.position.x = obs_pos.x
+    # po_obs.position.y = obs_pos.y
+    po_obs.pose.position.x = obs_pos.point.y  ## change x and y to plot
+    po_obs.pose.position.y = obs_pos.point.x
+    print obs_pos.point.y, obs_pos.point.x
+
+    path_obs.poses.append(po_obs)
+    path_obs.header.frame_id = 'WTST'
+    path_obs.header = obs_pos.header
+    path_pub_obs.publish(path_obs)
+
+
+
+def main():
+    global path_pub_ego, path_pub_obs, points_pub
+    rospy.init_node('get_path', anonymous=True)
+
+    points_pub = rospy.Publisher('/target_points', Marker, queue_size=2)
+    path_pub_ego = rospy.Publisher('/path_ego', Path, queue_size=2)
+    path_pub_obs = rospy.Publisher('/path_obs', Path, queue_size=2)
+
+    # wtst_sub = message_filters.Subscriber('/wtst', WTST_msg)
+    # obs_pos_sub = message_filters.Subscriber('/obs_position', Point)
+    #
+    # ts = message_filters.ApproximateTimeSynchronizer([wtst_sub, obs_pos_sub], queue_size=10,
+    #                                                  slop=0.1)  ## slop defines the delay (in seconds) with which messages can be synchronized
+    # ts.registerCallback(callback)
+
+    rospy.Subscriber('/wtst', WTST_msg, ego_cb)
+    rospy.Subscriber('/obs_position', PointStamped, obs_cb)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+        rospy.spin()
+    except:
+        pass
