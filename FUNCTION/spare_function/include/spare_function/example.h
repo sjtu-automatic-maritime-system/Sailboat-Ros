7b54e203f59be63ae4943bcdee36e37c2631a110
hywel1994 <xiehq1994@163.com>
2017-09-27 13:14:58 +0800

    add spare_function

---
+++ FUNCTION/spare_function/include/spare_function/example.h
@@ -0,0 +1,8 @@
+//
+// Created by sjtu on 17-9-27.
+//
+
+#ifndef PROJECT_EXAMPLE_H
+#define PROJECT_EXAMPLE_H
+
+#endif //PROJECT_EXAMPLE_H
---
+++ FUNCTION/spare_function/src/example.cpp
@@ -0,0 +1,4 @@
+//
+// Created by sjtu on 17-9-27.
+//
+
