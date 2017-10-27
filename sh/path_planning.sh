49fc76533e38e5b4845b62d8dd41d692c3bfb41c
StarsMyDestination <xujianyun101@gmail.com>
2017-10-27 12:57:04 +0800

    modify sh

--- sh/path_planning.sh
+++ sh/path_planning.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-nohup rosrun sensor_onboat GPS_Talker2.py &
-sleep 2s
+#nohup rosrun sensor_onboat GPS_Talker2.py &
+#sleep 2s
 nohup rosrun path_planning_astar astar_ros &
 sleep 2s
 nohup rosrun traj_following traj_following_ros &
 sleep 2s
-nohup rosbag record --split --duration 5m -j -o ~/BAG/path_planning /wtst_pro /gps_2 /traj_following_out /planned_path &
+nohup rosbag record --split --duration 5m -j -o ~/BAG/path_planning /wtst_pro /traj_following_out /traj_following_para /planned_path &
 rosnode list
