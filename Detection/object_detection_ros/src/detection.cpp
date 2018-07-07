db812c02c1e50743cb9dfc975b58d0ab8618242c
hywel <xiehq1994@163.com>
2018-07-07 14:19:46 +0800

    modify object detection

--- Detection/object_detection_ros/src/detection.cpp
+++ Detection/object_detection_ros/src/detection.cpp
@@ -19,25 +19,28 @@ cv::Mat edgeDetection(cv::Mat img_src, u_int8_t low_threshold=100, u_int8_t high
     cvtColor(img_src, hls, CV_BGR2HLS);
     std::vector<cv::Mat> hls_channels;
     cv::split(hls, hls_channels);
-    cv::Mat h_img = hls_channels[0];
+    //cv::Mat h_img = hls_channels[0];
     //imshow("hue", h_img);
     //cv::waitKey(5);
-    cv::Mat l_img = hls_channels[1];
+    //cv::Mat l_img = hls_channels[1];
     //imshow("lightness", l_img);
     //cv::waitKey(0);
-    cv::Mat s_img = hls_channels[2];
+    //cv::Mat s_img = hls_channels[2];
     //imshow("saturation", s_img);
+    cv::Mat output = hls_channels[2].mul(hls_channels[1]/255);
+    //imshow("output", output);
     cv::waitKey(5);
 
-    cv::GaussianBlur(s_img, edge, cv::Size(9, 9), 2, 2);
+    cv::GaussianBlur(output, edge, cv::Size(9, 9), 2, 2);
 
     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
 
-    cv::morphologyEx(edge,edge, cv::MORPH_OPEN, kernel);
+    cv::morphologyEx(edge, edge, cv::MORPH_OPEN, kernel);
+    cv::morphologyEx(edge, edge, CV_MOP_CLOSE, kernel);
 
-    cv::threshold(edge,edge, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
+    //cv::threshold(edge,edge, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
 
-    cv::Canny(edge, edge, low_threshold, high_threshold, 3);
+    //cv::Canny(edge, edge, low_threshold, high_threshold, 3);
 
     // cv::morphologyEx(edge, edge, CV_MOP_CLOSE, kernel);
     // cv::morphologyEx(edge, edge, cv::MORPH_OPEN, kernel);
@@ -57,7 +60,7 @@ std::vector<cv::Vec3f> circleDectection(cv::Mat &src_img, cv::Mat edge) {
     //cv::Mat gray;
 
     std::vector<cv::Vec3f> circles;
-    cv::HoughCircles(edge, circles, CV_HOUGH_GRADIENT, 2, 100, 200, 100, 20, 300);
+    cv::HoughCircles(edge, circles, CV_HOUGH_GRADIENT, 2, 100, 200, 100, 0, 0);
 
     for (size_t i = 0; i < circles.size(); i++) {
         cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
