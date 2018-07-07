9123df90c0e82c9314b2b6334be67eab0d0ab2a9
hywel <xiehq1994@163.com>
2018-07-08 02:12:26 +0800

    modify object detection

--- Detection/object_detection_ros/src/detection.cpp
+++ Detection/object_detection_ros/src/detection.cpp
@@ -10,57 +10,68 @@
 namespace detection {
 cv::Mat edgeDetection(cv::Mat img_src, u_int8_t low_threshold=100, u_int8_t high_threshold=200) {
 //    cv::Mat img_clone = img_src.clone();
-    cv::Mat dst, edge, hls;
+    cv::Mat gray, hsv;
+    //imshow("src", img_src);
 
-    dst.create(img_src.size(), img_src.type());
+    int iLowH = 156;
+    int iHighH = 180;
+    int iLowS = 43;
+    int iHighS = 255;
+    int iLowV = 46;
+    int iHighV = 255;
 
-    cvtColor(img_src, edge, CV_BGR2GRAY);
+    cvtColor(img_src, gray, CV_BGR2GRAY);
+    //imshow("edge0", edge);
 
-    cvtColor(img_src, hls, CV_BGR2HLS);
-    std::vector<cv::Mat> hls_channels;
-    cv::split(hls, hls_channels);
-    //cv::Mat h_img = hls_channels[0];
-    //imshow("hue", h_img);
-    //cv::waitKey(5);
-    //cv::Mat l_img = hls_channels[1];
-    //imshow("lightness", l_img);
-    //cv::waitKey(0);
-    //cv::Mat s_img = hls_channels[2];
+    cvtColor(img_src, hsv, CV_BGR2HSV);
+    
+    //imshow("hsv", hsv);
+
+    std::vector<cv::Mat> hsv_channels;
+    cv::split(hsv, hsv_channels);
+
+    cv::Mat h_img = hsv_channels[0];
+   // imshow("hue", h_img);
+
+    cv::Mat s_img = hsv_channels[1];
     //imshow("saturation", s_img);
-    cv::Mat output = hls_channels[2].mul(hls_channels[1]/255);
-    //imshow("output", output);
-    cv::waitKey(5);
 
-    cv::GaussianBlur(output, edge, cv::Size(9, 9), 2, 2);
+    cv::Mat v_img = hsv_channels[2];
+    //imshow("v", v_img);
+    
+    cv::equalizeHist(hsv_channels[2], hsv_channels[2]);
+    cv::merge(hsv_channels, hsv);
 
-    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+    //imshow("merge", hsv);
+    
+    cv::inRange(hsv, cv::Scalar(iLowH,iLowS,iLowV),cv::Scalar(iHighH,iHighS,iHighV),gray);
+    //edge = hls_channels[2];//mul(hls_channels[1]*2/255);
+    //imshow("output", gray);
 
-    cv::morphologyEx(edge, edge, cv::MORPH_OPEN, kernel);
-    cv::morphologyEx(edge, edge, CV_MOP_CLOSE, kernel);
+    cv::GaussianBlur(gray, gray, cv::Size(5, 5), 1, 1);
 
-    //cv::threshold(edge,edge, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
+    // cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
 
-    //cv::Canny(edge, edge, low_threshold, high_threshold, 3);
+    // cv::morphologyEx(gray, gray, cv::MORPH_OPEN, kernel);
+    // cv::morphologyEx(gray, gray, cv::MORPH_CLOSE, kernel);
 
-    // cv::morphologyEx(edge, edge, CV_MOP_CLOSE, kernel);
-    // cv::morphologyEx(edge, edge, cv::MORPH_OPEN, kernel);
+    //imshow("morphologyEx", edge);
 
-    dst = cv::Scalar::all(0);
-
-    img_src.copyTo(dst, edge);
-
-    //imshow("dst", dst);
-    //cv::waitKey(0);
-
-    return edge;
+    //cv::Canny(edge, edge, 100, 200, 3);
+    //imshow("Canny", edge);
+    return gray;
 }
-
 
 std::vector<cv::Vec3f> circleDectection(cv::Mat &src_img, cv::Mat edge) {
     //cv::Mat gray;
 
     std::vector<cv::Vec3f> circles;
-    cv::HoughCircles(edge, circles, CV_HOUGH_GRADIENT, 2, 100, 200, 100, 0, 0);
+    cv::HoughCircles(edge, circles, CV_HOUGH_GRADIENT, 2, 100, 200, 100);
+
+    if (circles.size()>10){
+        std::vector<cv::Vec3f> circleNone;
+        return circleNone;
+    }
 
     for (size_t i = 0; i < circles.size(); i++) {
         cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
@@ -68,7 +79,7 @@ std::vector<cv::Vec3f> circleDectection(cv::Mat &src_img, cv::Mat edge) {
         cv::circle(src_img, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
         cv::circle(src_img, center, radius, cv::Scalar(155, 50, 255), 3, 8, 0);
     }
-    //cv::imshow("hough circle", edge);
+    //cv::imshow("hough circle", src_img);
     //cv::waitKey(0);
     return circles;
 }
--- Sh/onboat/rosbag.sh
+++ Sh/onboat/rosbag.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 nohup rosbag record --split --duration 5m -j -o /home/sjtu/rosbag -a &
+#/ahrs /wtst /mach /base/mach /arduino /sensor /sensor2 /sensor_kalman_msg &
 #nohup rosbag record --split --duration 5m -j -o ~/rosbag/camera /camera/image_raw &
