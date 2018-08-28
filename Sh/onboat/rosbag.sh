4624360967bde1eac434eef5975ca8ffaca20981
hywel <xiehq1994@163.com>
2018-08-28 16:40:38 +0800

    modify sh

--- Sh/onboat/rosbag.sh
+++ Sh/onboat/rosbag.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-nohup rosbag record --split --duration 60m -j -o /home/sjtu151/rosbag /ahrs /wtst /mach /base/mach /arduino /sensor /sensor2 /sensor_kalman_msg &
+nohup rosbag record --split --duration 60m -j -o /home/sjtu151/rosbag /ahrs /wtst /mach /base/mach /arduino /sensor_kalman_msg /station_keeping_out /fleet_race_out /scanning_out &
 #nohup rosbag record --split --duration 5m -j -o ~/rosbag/camera /camera/image_raw &
